25308d7de26bc10121e407ced2c1b40bf6b391bc
Mohammed Masood Ansari <45538699+Mohammed-Masood-Ansari@users.noreply.github.com>
2023-10-17 14:34:14 +0000

    string pre

---
+++ 02-functions/program2.js
@@ -0,0 +1,9 @@
+let myFirstName = "Mo"
+let myMiddleName = "Masood"
+let myLastName = "Ansari"
+
+console.log(`firstName = ${myFirstName} lastName = ${myLastName}`);
+
+const dataGame = new String('Masood Ansari');
+
+console.log(dataGame[0]);
