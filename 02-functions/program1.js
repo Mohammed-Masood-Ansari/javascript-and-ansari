97dc5b39b968a5d980377409bce755026d9a2f04
Mohammed Masood Ansari <45538699+Mohammed-Masood-Ansari@users.noreply.github.com>
2023-10-17 14:13:28 +0000

    function

---
+++ 02-functions/program1.js
@@ -0,0 +1,14 @@
+const myFunction = function(){
+    console.log("my first function");
+}
+
+console.log(myFunction.myFunction);
+
+let userOne = {
+    "userId":"1234",
+    "userName":"ANSARI"
+}
+
+console.log(userOne.userId)
+console.log(userOne.userName)
+
